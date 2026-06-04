5dceacc583b5ca14cb410eea9780b2ccada5dd72
PChemGuy <39730837+pchemguy@users.noreply.github.com>
2026-06-04 17:26:31 +0300

    Update junctions.bat

--- Profile Skeleton/PROFILE/junctions.bat
+++ Profile Skeleton/PROFILE/junctions.bat
@@ -1,5 +1,7 @@
 @echo off
 
+pushd "%CD%"
+cd /d "%~dp0"
 
 for /d %%D in ("USERPROFILE\*") do (
     set "SRC=%%~D"
@@ -16,3 +18,5 @@ for /d %%D in ("APPDATA\*") do (
     if exist "!DST!" rmdir /S /Q "!DST!"
     mklink /j "!DST!" "!SRC!"
 )
+
+popd
